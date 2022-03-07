39c80c7de1d4323067454d285b5d76f10e7dc8ab
Liujian <824010343@qq.com>
2022-03-07 18:41:17 +0800

    调整初始化配置

---
+++ pkg/config/config.go
@@ -0,0 +1,223 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/validation"
+)
+
+const (
+	// NamespaceAll represents all namespaces.
+	NamespaceAll = "*"
+	// IngressClass is the default ingress class name, used for Ingress
+	// object's IngressClassName field in Kubernetes clusters version v1.18.0
+	// or higher, or the annotation "kubernetes.io/ingress.class" (deprecated).
+	IngressClass = "apinto"
+
+	// IngressNetworkingV1 represents ingress.networking/v1
+	IngressNetworkingV1 = "networking/v1"
+	// IngressNetworkingV1beta1 represents ingress.networking/v1beta1
+	IngressNetworkingV1beta1 = "networking/v1beta1"
+	// IngressExtensionsV1beta1 represents ingress.extensions/v1beta1
+	// WARNING: ingress.extensions/v1beta1 is deprecated in v1.14+, and will be unavilable
+	// in v1.22.
+	IngressExtensionsV1beta1 = "extensions/v1beta1"
+)
+
+// Config contains all config items which are necessary for
+// apisix-ingress-controller's running.
+type Config struct {
+	CertFilePath          string           `json:"cert_file" yaml:"cert_file"`
+	KeyFilePath           string           `json:"key_file" yaml:"key_file"`
+	LogLevel              string           `json:"log_level" yaml:"log_level"`
+	LogOutput             string           `json:"log_output" yaml:"log_output"`
+	HTTPListen            string           `json:"http_listen" yaml:"http_listen"`
+	HTTPSListen           string           `json:"https_listen" yaml:"https_listen"`
+	IngressPublishService string           `json:"ingress_publish_service" yaml:"ingress_publish_service"`
+	IngressStatusAddress  []string         `json:"ingress_status_address" yaml:"ingress_status_address"`
+	EnableProfiling       bool             `json:"enable_profiling" yaml:"enable_profiling"`
+	Kubernetes            KubernetesConfig `json:"kubernetes" yaml:"kubernetes"`
+	APINTO                APINTOConfig     `json:"apinto" yaml:"apinto"`
+}
+
+// KubernetesConfig contains all Kubernetes related config items.
+type KubernetesConfig struct {
+	Kubeconfig        string   `json:"kubeconfig" yaml:"kubeconfig"`
+	AppNamespaces     []string `json:"app_namespaces" yaml:"app_namespaces"`
+	NamespaceSelector []string `json:"namespace_selector" yaml:"namespace_selector"`
+	IngressClass      string   `json:"ingress_class" yaml:"ingress_class"`
+	IngressVersion    string   `json:"ingress_version" yaml:"ingress_version"`
+}
+
+// APINTOConfig contains all APINTO related config items.
+type APINTOConfig struct {
+	// DefaultClusterBaseURL is the base url configuration for the default cluster.
+	DefaultClusterBaseURL string `json:"default_cluster_base_url" yaml:"default_cluster_base_url"`
+	// DefaultClusterAdminKey is the admin key for the default cluster.
+	// TODO: Obsolete the plain way to specify admin_key, which is insecure.
+	DefaultClusterAdminKey string `json:"default_cluster_admin_key" yaml:"default_cluster_admin_key"`
+}
+
+// NewDefaultConfig creates a Config object which fills all config items with
+// default value.
+func NewDefaultConfig() *Config {
+	return &Config{
+		LogLevel:              "warn",
+		LogOutput:             "stderr",
+		HTTPListen:            ":8080",
+		HTTPSListen:           ":8443",
+		IngressPublishService: "",
+		IngressStatusAddress:  []string{},
+		CertFilePath:          "/etc/webhook/certs/cert.pem",
+		KeyFilePath:           "/etc/webhook/certs/key.pem",
+		EnableProfiling:       true,
+		Kubernetes: KubernetesConfig{
+			Kubeconfig:     "", // Use in-cluster configurations.
+			AppNamespaces:  []string{v1.NamespaceAll},
+			IngressClass:   IngressClass,
+			IngressVersion: IngressNetworkingV1,
+		},
+	}
+}
+
+// NewConfigFromFile creates a Config object and fills all config items according
+// to the configuration file. The file can be in JSON/YAML format, which will be
+// distinguished according to the file suffix.
+func NewConfigFromFile(filename string) (*Config, error) {
+	cfg := NewDefaultConfig()
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	envVarMap := map[string]string{}
+	for _, e := range os.Environ() {
+		pair := strings.SplitN(e, "=", 2)
+		envVarMap[pair[0]] = pair[1]
+	}
+
+	tpl := template.New("text").Option("missingkey=error")
+	tpl, err = tpl.Parse(string(data))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing configuration template %v", err)
+	}
+	buf := bytes.NewBufferString("")
+	err = tpl.Execute(buf, envVarMap)
+	if err != nil {
+		return nil, fmt.Errorf("error execute configuration template %v", err)
+	}
+
+	if strings.HasSuffix(filename, ".yaml") || strings.HasSuffix(filename, ".yml") {
+		err = yaml.Unmarshal(buf.Bytes(), cfg)
+	} else {
+		err = json.Unmarshal(buf.Bytes(), cfg)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+// Validate validates whether the Config is right.
+func (cfg *Config) Validate() error {
+
+	if cfg.APINTO.DefaultClusterBaseURL == "" {
+		return errors.New("apisix base url is required")
+	}
+	switch cfg.Kubernetes.IngressVersion {
+	case IngressNetworkingV1, IngressNetworkingV1beta1, IngressExtensionsV1beta1:
+		break
+	default:
+		return errors.New("unsupported ingress version")
+	}
+	cfg.Kubernetes.AppNamespaces = purifyAppNamespaces(cfg.Kubernetes.AppNamespaces)
+	ok, err := cfg.verifyNamespaceSelector()
+	if !ok {
+		return err
+	}
+	return nil
+}
+
+func purifyAppNamespaces(namespaces []string) []string {
+	exists := make(map[string]struct{})
+	var ultimate []string
+	for _, ns := range namespaces {
+		if ns == NamespaceAll {
+			return []string{v1.NamespaceAll}
+		}
+		if _, ok := exists[ns]; !ok {
+			ultimate = append(ultimate, ns)
+			exists[ns] = struct{}{}
+		}
+	}
+	return ultimate
+}
+
+func (cfg *Config) verifyNamespaceSelector() (bool, error) {
+	labels := cfg.Kubernetes.NamespaceSelector
+	// default is [""]
+	if len(labels) == 1 && labels[0] == "" {
+		cfg.Kubernetes.NamespaceSelector = []string{}
+	}
+
+	for _, s := range cfg.Kubernetes.NamespaceSelector {
+		parts := strings.Split(s, "=")
+		if len(parts) != 2 {
+			return false, fmt.Errorf("Illegal namespaceSelector: %s, should be key-value pairs divided by = ", s)
+		} else {
+			if err := cfg.validateLabelKey(parts[0]); err != nil {
+				return false, err
+			}
+			if err := cfg.validateLabelValue(parts[1]); err != nil {
+				return false, err
+			}
+		}
+	}
+	return true, nil
+}
+
+// validateLabelKey validate the key part of label
+// ref: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
+func (cfg *Config) validateLabelKey(key string) error {
+	errorMsg := validation.IsQualifiedName(key)
+	msg := strings.Join(errorMsg, "\n")
+	if msg == "" {
+		return nil
+	}
+	return fmt.Errorf("Illegal namespaceSelector: %s, "+msg, key)
+}
+
+// validateLabelValue validate the value part of label
+// ref: https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/#syntax-and-character-set
+func (cfg *Config) validateLabelValue(value string) error {
+	errorMsg := validation.IsValidLabelValue(value)
+	msg := strings.Join(errorMsg, "\n")
+	if msg == "" {
+		return nil
+	}
+	return fmt.Errorf("Illegal namespaceSelector: %s, "+msg, value)
+}
---
+++ pkg/config/config_test.go
@@ -0,0 +1,270 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The ASF licenses this file to You under the Apache License, Version 2.0
+// (the "License"); you may not use this file except in compliance with
+// the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConfigFromFile(t *testing.T) {
+	cfg := &Config{
+		LogLevel:              "warn",
+		LogOutput:             "stdout",
+		HTTPListen:            ":9090",
+		HTTPSListen:           ":9443",
+		IngressPublishService: "",
+		IngressStatusAddress:  []string{},
+		CertFilePath:          "/etc/webhook/certs/cert.pem",
+		KeyFilePath:           "/etc/webhook/certs/key.pem",
+		EnableProfiling:       true,
+		Kubernetes: KubernetesConfig{
+			Kubeconfig:     "/path/to/foo/baz",
+			AppNamespaces:  []string{""},
+			IngressClass:   IngressClass,
+			IngressVersion: IngressNetworkingV1,
+		},
+		APINTO: APINTOConfig{
+			DefaultClusterBaseURL:  "http://127.0.0.1:8080/apisix",
+			DefaultClusterAdminKey: "123456",
+		},
+	}
+
+	jsonData, err := json.Marshal(cfg)
+	assert.Nil(t, err, "failed to marshal config to json: %s", err)
+
+	tmpJSON, err := ioutil.TempFile("/tmp", "config-*.json")
+	assert.Nil(t, err, "failed to create temporary json configuration file: ", err)
+	defer os.Remove(tmpJSON.Name())
+
+	_, err = tmpJSON.Write(jsonData)
+	assert.Nil(t, err, "failed to write json data: ", err)
+	tmpJSON.Close()
+
+	newCfg, err := NewConfigFromFile(tmpJSON.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	assert.Nil(t, newCfg.Validate(), "failed to validate config")
+
+	assert.Equal(t, cfg, newCfg, "bad configuration")
+
+	// We constructs yaml data manually instead of using yaml.Marshal since
+	// types.TimeDuration doesn't have a `yaml:",inline"` tag, if we add it,
+	// error ",inline needs a struct value field" will be reported.
+	// I don't know why.
+	yamlData := `
+log_level: warn
+log_output: stdout
+http_listen: :9090
+https_listen: :9443
+ingress_publish_service: ""
+ingress_status_address: []
+enable_profiling: true
+kubernetes:
+  kubeconfig: /path/to/foo/baz
+  resync_interval: 1h0m0s
+  election_id: my-election-id
+  ingress_class: apisix
+  ingress_version: networking/v1
+apisix:
+  default_cluster_base_url: http://127.0.0.1:8080/apisix
+  default_cluster_admin_key: "123456"
+`
+	tmpYAML, err := ioutil.TempFile("/tmp", "config-*.yaml")
+	assert.Nil(t, err, "failed to create temporary yaml configuration file: ", err)
+	defer os.Remove(tmpYAML.Name())
+
+	_, err = tmpYAML.Write([]byte(yamlData))
+	assert.Nil(t, err, "failed to write yaml data: ", err)
+	tmpYAML.Close()
+
+	newCfg, err = NewConfigFromFile(tmpYAML.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	assert.Nil(t, newCfg.Validate(), "failed to validate config")
+
+	assert.Equal(t, cfg, newCfg, "bad configuration")
+}
+
+func TestConfigWithEnvVar(t *testing.T) {
+	cfg := &Config{
+		LogLevel:              "warn",
+		LogOutput:             "stdout",
+		HTTPListen:            ":9090",
+		HTTPSListen:           ":9443",
+		IngressPublishService: "",
+		IngressStatusAddress:  []string{},
+		CertFilePath:          "/etc/webhook/certs/cert.pem",
+		KeyFilePath:           "/etc/webhook/certs/key.pem",
+		EnableProfiling:       true,
+		Kubernetes: KubernetesConfig{
+			Kubeconfig:     "",
+			AppNamespaces:  []string{""},
+			IngressClass:   IngressClass,
+			IngressVersion: IngressNetworkingV1,
+		},
+		APINTO: APINTOConfig{
+			DefaultClusterBaseURL:  "http://127.0.0.1:8080/apisix",
+			DefaultClusterAdminKey: "123456",
+		},
+	}
+
+	defaultClusterBaseURLEnvName := "DEFAULT_CLUSTER_BASE_URL"
+	defaultClusterAdminKeyEnvName := "DEFAULT_CLUSTER_ADMIN_KEY"
+	kubeconfigEnvName := "KUBECONFIG"
+
+	err := os.Setenv(defaultClusterBaseURLEnvName, "http://127.0.0.1:8080/apisix")
+	assert.Nil(t, err, "failed to set env variable: ", err)
+	_ = os.Setenv(defaultClusterAdminKeyEnvName, "123456")
+	_ = os.Setenv(kubeconfigEnvName, "")
+
+	jsonData := `
+{
+    "log_level": "warn",
+    "log_output": "stdout",
+    "http_listen": ":9090",
+    "https_listen": ":9443",
+	"ingress_publish_service": "",
+	"ingress_status_address": [],
+    "enable_profiling": true,
+    "kubernetes": {
+        "kubeconfig": "{{.KUBECONFIG}}",
+        "resync_interval": "1h0m0s",
+        "election_id": "my-election-id",
+        "ingress_class": "apisix",
+        "ingress_version": "networking/v1"
+    },
+    "apisix": {
+        "default_cluster_base_url": "{{.DEFAULT_CLUSTER_BASE_URL}}",
+        "default_cluster_admin_key": "{{.DEFAULT_CLUSTER_ADMIN_KEY}}"
+    }
+}
+`
+	tmpJSON, err := ioutil.TempFile("/tmp", "config-*.json")
+	assert.Nil(t, err, "failed to create temporary json configuration file: ", err)
+	defer os.Remove(tmpJSON.Name())
+
+	_, err = tmpJSON.Write([]byte(jsonData))
+	assert.Nil(t, err, "failed to write json data: ", err)
+	tmpJSON.Close()
+
+	newCfg, err := NewConfigFromFile(tmpJSON.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	assert.Nil(t, newCfg.Validate(), "failed to validate config")
+
+	assert.Equal(t, cfg, newCfg, "bad configuration")
+
+	yamlData := `
+log_level: warn
+log_output: stdout
+http_listen: :9090
+https_listen: :9443
+ingress_publish_service: ""
+ingress_status_address: []
+enable_profiling: true
+kubernetes:
+  resync_interval: 1h0m0s
+  kubeconfig: "{{.KUBECONFIG}}"
+  election_id: my-election-id
+  ingress_class: apisix
+  ingress_version: networking/v1
+apisix:
+  default_cluster_base_url: {{.DEFAULT_CLUSTER_BASE_URL}}
+  default_cluster_admin_key: "{{.DEFAULT_CLUSTER_ADMIN_KEY}}"
+`
+	tmpYAML, err := ioutil.TempFile("/tmp", "config-*.yaml")
+	assert.Nil(t, err, "failed to create temporary yaml configuration file: ", err)
+	defer os.Remove(tmpYAML.Name())
+
+	_, err = tmpYAML.Write([]byte(yamlData))
+	assert.Nil(t, err, "failed to write yaml data: ", err)
+	tmpYAML.Close()
+
+	newCfg, err = NewConfigFromFile(tmpYAML.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	assert.Nil(t, newCfg.Validate(), "failed to validate config")
+
+	assert.Equal(t, cfg, newCfg, "bad configuration")
+
+	_ = os.Unsetenv(defaultClusterBaseURLEnvName)
+
+	_, err = NewConfigFromFile(tmpJSON.Name())
+	assert.NotNil(t, err, "should failed because env variable missing")
+
+	_, err = NewConfigFromFile(tmpYAML.Name())
+	assert.NotNil(t, err, "should failed because env variable missing")
+}
+
+func TestConfigDefaultValue(t *testing.T) {
+	yamlData := `
+apisix:
+  default_cluster_base_url: http://127.0.0.1:8080/apisix
+`
+	tmpYAML, err := ioutil.TempFile("/tmp", "config-*.yaml")
+	assert.Nil(t, err, "failed to create temporary yaml configuration file: ", err)
+	defer os.Remove(tmpYAML.Name())
+
+	_, err = tmpYAML.Write([]byte(yamlData))
+	assert.Nil(t, err, "failed to write yaml data: ", err)
+	tmpYAML.Close()
+
+	newCfg, err := NewConfigFromFile(tmpYAML.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	assert.Nil(t, newCfg.Validate(), "failed to validate config")
+
+	defaultCfg := NewDefaultConfig()
+	defaultCfg.APINTO.DefaultClusterBaseURL = "http://127.0.0.1:8080/apisix"
+
+	assert.Equal(t, defaultCfg, newCfg, "bad configuration")
+}
+
+func TestConfigInvalidation(t *testing.T) {
+	yamlData := ``
+	tmpYAML, err := ioutil.TempFile("/tmp", "config-*.yaml")
+	assert.Nil(t, err, "failed to create temporary yaml configuration file: ", err)
+	defer os.Remove(tmpYAML.Name())
+
+	_, err = tmpYAML.Write([]byte(yamlData))
+	assert.Nil(t, err, "failed to write yaml data: ", err)
+	tmpYAML.Close()
+
+	newCfg, err := NewConfigFromFile(tmpYAML.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	err = newCfg.Validate()
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "apisix base url is required", "bad error: ", err)
+
+	yamlData = `
+kubernetes:
+  resync_interval: 15s
+apisix:
+  default_cluster_base_url: http://127.0.0.1:1234/apisix
+`
+	tmpYAML, err = ioutil.TempFile("/tmp", "config-*.yaml")
+	assert.Nil(t, err, "failed to create temporary yaml configuration file: ", err)
+	defer os.Remove(tmpYAML.Name())
+
+	_, err = tmpYAML.Write([]byte(yamlData))
+	assert.Nil(t, err, "failed to write yaml data: ", err)
+	tmpYAML.Close()
+
+	newCfg, err = NewConfigFromFile(tmpYAML.Name())
+	assert.Nil(t, err, "failed to new config from file: ", err)
+	err = newCfg.Validate()
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "controller resync interval too small", "bad error: ", err)
+}
