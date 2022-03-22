6397dca853c8c807fea0429cb0fbaa7991acbf5e
zeke <526756656@qq.com>
2022-03-22 15:58:57 +0800

    update admission init method

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -3,19 +3,20 @@ package api
 import (
 	"crypto/tls"
 	"github.com/eolinker/apinto-ingress-controller/pkg/api/router"
+	"github.com/eolinker/apinto-ingress-controller/pkg/apinto"
 	"github.com/eolinker/apinto-ingress-controller/pkg/config"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func NewAdmissionServer(cfg *config.Config) (*http.Server, error) {
+func NewAdmissionServer(cfg *config.Config, apinto apinto.Apinto) (*http.Server, error) {
 	cert, err := tls.LoadX509KeyPair(cfg.CertFilePath, cfg.KeyFilePath)
 	if err != nil {
 		return nil, err
 	} else {
 		admission := gin.New()
 		admission.Use(gin.Recovery(), gin.Logger())
-		err = router.ValidatingWebhook(admission, cfg)
+		err = router.ValidatingWebhook(admission, cfg, apinto)
 		if err != nil {
 			return nil, err
 		}
